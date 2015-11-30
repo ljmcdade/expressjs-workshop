6b30920041cb2a619a19660127454935753ba11d
ljmcdade <ljmcdade@gmail.com>
2015-11-30 22:09:35 +0000

    exercise-3-operations

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+app.get('/op/:operation/:num1/:num2', function(request, response) {
+
+  var add = Number(request.params.num1) + Number(request.params.num2);
+  var sub = Number(request.params.num1) - Number(request.params.num2);
+  var mult = Number(request.params.num1) * Number(request.params.num2);
+  var div = Number(request.params.num1) / Number(request.params.num2);
+
+  var result = {
+
+    "operator": request.params.operation,
+    "firstOperand": request.params.num1,
+    "secondOperand": request.params.num2,
+    "solution": ''
+
+  };
+
+  if (request.params.operation === 'add') {
+    result.solution = add;
+    response.json(result);
+  }
+  else if (request.params.operation === 'sub') {
+    result.solution = sub;
+    response.json(result);
+  }
+  else if (request.params.operation === 'mult') {
+    result.solution = mult;
+    response.json(result);
+  }
+  else if (request.params.operation === 'div') {
+    result.solution = div;
+    response.json(result);
+  }
+  else if (result.solution === '') {
+    response.send('403');
+  }
 });
 
 
@@ -12,7 +46,7 @@ app.get('/', function (req, res) {
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
 // Boilerplate code to start up the web server
-var server = app.listen(process.env.PORT, process.env.IP, function () {
+var server = app.listen(process.env.PORT, process.env.IP, function() {
   var host = server.address().address;
   var port = server.address().port;
 
