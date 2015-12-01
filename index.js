a13ea2ddff17097a3ed2a480053f8629c14833d8
ljmcdade <ljmcdade@gmail.com>
2015-12-01 00:40:50 +0000

    exercise-5-searching-for-data

--- index.js
+++ index.js
@@ -1,9 +1,87 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var entries = {
+  1: {
+    firstName: "Karl",
+    lastName: "Marx",
+    emails: [{
+      type: "home",
+      address: "karl@communist.com"
+    }, {
+      type: "work",
+      address: "ihateto@work.com"
+    }]
+  },
+  2: {
+    firstName: "Edith",
+    lastName: "Wharton",
+    emails: [{
+      type: "home",
+      address: "edith@themanse.com"
+    }, {
+      type: "work",
+      address: "misswharton@writer.com"
+    }]
+  },
+  3: {
+    firstName: "Martin",
+    lastName: "Amis",
+    emails: [{
+      type: "home",
+      address: "smartymarty@nicerthanyours.com"
+    }, {
+      type: "work",
+      address: "notmydad@britwriter.com"
+    }]
+  },
+  4: {
+    firstName: "Louisa May",
+    lastName: "Alcott",
+    emails: [{
+      type: "home",
+      address: "lm@alcotthouse.com"
+    }, {
+      type: "work",
+      address: "lmalcott@concord.com"
+    }]
+  },
+  5: {
+    firstName: "Ralph Waldo",
+    lastName: "Emerson",
+    emails: [{
+      type: "home",
+      address: "ralph@oldmanse.com"
+    }, {
+      type: "work",
+      address: "rwemerson@concord.com"
+    }]
+  },
+};
+
+var results = [];
+
+app.get('/entry/search', function(req, res) {
+
+  for (var entryId in entries) {
+
+    var lastName = req.query.lastName === entries[entryId].lastName;
+    var firstName = req.query.firstName === entries[entryId].firstName;
+    var emails = req.query.emails === entries[entryId].emails;
+
+    if (lastName || firstName || emails) {
+      results.push(entries[entryId]);
+    }
+  }
+  res.json(results);
 });
+
+
+
+
+
+
+
 
 
 
@@ -12,7 +90,7 @@ app.get('/', function (req, res) {
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
 // Boilerplate code to start up the web server
-var server = app.listen(process.env.PORT, process.env.IP, function () {
+var server = app.listen(process.env.PORT, process.env.IP, function() {
   var host = server.address().address;
   var port = server.address().port;
 
